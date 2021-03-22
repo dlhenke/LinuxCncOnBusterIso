a9df3c880767e68ed59d8b758ce51ec668bec375
Durval Luis Henke <durvalhenke@gmail.com>
2021-03-22 12:08:13 -0300

    Update install_linux_cnc_rt_preempt_buster.sh

--- install_linux_cnc_rt_preempt_buster.sh
+++ install_linux_cnc_rt_preempt_buster.sh
@@ -6,7 +6,12 @@ FILE=/etc/apt/sources.dlh.orig
 CNCFILE=/etc/apt/sources.list.d/linuxcnc.list
 if [ ! -f "$FILE" ]; then
     sudo mv /etc/apt/sources.list /etc/apt/sources.dlh.orig
-    sudo echo -e "deb http://deb.debian.org/debian buster main contrib non-free\ndeb-src http://deb.debian.org/debian buster main contrib non-free\ndeb http:$
+    sudo echo -e "deb http://deb.debian.org/debian buster main contrib non-free" > /etc/apt/sources.list
+    sudo echo -e "deb-src http://deb.debian.org/debian buster main contrib non-free" >> /etc/apt/sources.list
+    sudo echo -e "deb http://deb.debian.org/debian buster-updates main contrib non-free" >> /etc/apt/sources.list
+    sudo echo -e "deb-src http://deb.debian.org/debian buster-updates main contrib non-free" >> /etc/apt/sources.list
+    sudo echo -e "deb http://security.debian.org/debian-security/ buster/updates main contrib non-free" >> /etc/apt/sources.list
+    sudo echo -e "deb-src http://security.debian.org/debian-security/ buster/updates main contrib non-free" >> /etc/apt/sources.list
     sudo apt-get -y update
     sudo apt-get -y upgrade
     sudo apt-get -y install gnome-terminal
@@ -15,9 +20,11 @@ if [ ! -f "$FILE" ]; then
     sudo apt-get -y install firmware-linux-nonfree firmware-realtek
 fi
 if [ ! -f "$CNCFILE" ]; then
-    sudo echo -e "deb http://linuxcnc.org/ buster base 2.8-rtpreempt">/etc/apt/sources.list.d/linuxcnc.list
+    echo -e "deb http://linuxcnc.org/ buster base 2.8-rtpreempt">/etc/apt/sources.list.d/linuxcnc.list
     sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-key 3cb9fd148f374fef
     sudo apt-get update
     sudo apt-get -y install linuxcnc-uspace
     sudo apt-get -y install linuxcnc-uspace-dev
 fi
+
+sudo apt remove -y xterm xiterm+thai mlterm kasumi
