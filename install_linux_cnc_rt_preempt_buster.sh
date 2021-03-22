6cde59e56b9732c677b89e8b2fafd32970f84286
Durval Luis Henke <durvalhenke@gmail.com>
2021-03-22 11:39:34 -0300

    Update install_linux_cnc_rt_preempt_buster.sh

--- install_linux_cnc_rt_preempt_buster.sh
+++ install_linux_cnc_rt_preempt_buster.sh
@@ -15,7 +15,7 @@ if [ ! -f "$FILE" ]; then
     sudo apt-get -y install firmware-linux-nonfree firmware-realtek
 fi
 if [ ! -f "$CNCFILE" ]; then
-    sueo echo -e "deb http://linuxcnc.org/ buster base 2.8-rtpreempt">/etc/apt/sources.list.d/linuxcnc.list
+    sudo echo -e "deb http://linuxcnc.org/ buster base 2.8-rtpreempt">/etc/apt/sources.list.d/linuxcnc.list
     sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-key 3cb9fd148f374fef
     sudo apt-get update
     sudo apt-get -y install linuxcnc-uspace
